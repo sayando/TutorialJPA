1d0e3457c72a5b1f4fcfa87c5a5fb812f409fcc0
sayando <andousayuka@gmail.com>
2023-05-02 20:15:14 +0900

    kadai

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("country")
 public class CountryController {
+
     private final CountryService service;
 
     public CountryController(CountryService service) {
@@ -23,8 +24,8 @@ public class CountryController {
         // 全件検索結果をModelに登録
         model.addAttribute("countrylist", service.getCountryList());
         // 「詳細」と「削除」へのリンクを追加
-        model.addAttribute("detailUrl", "/country/detail");
-        model.addAttribute("deleteUrl", "/country/delete");
+        //model.addAttribute("detailUrl", "/country/detail");
+        //model.addAttribute("deleteUrl", "/country/delete");
         // country/list.htmlに画面遷移
         return "country/list";
     }
@@ -37,7 +38,7 @@ public class CountryController {
         // Modelに登録
         model.addAttribute("country", country);
         // 詳細画面から一覧画面に戻るリンクを追加
-        model.addAttribute("backToListUrl", "/country/list");
+        //model.addAttribute("backToListUrl", "/country/list");
         // country/detail.htmlに画面遷移
         return "country/detail";
     }
@@ -48,13 +49,12 @@ public class CountryController {
             @RequestParam("population") int population, Model model) {
         // 更新（追加）
         service.updateCountry(code, name, population);
-
         // 一覧画面にリダイレクト
         return "redirect:/country/list";
     }
 
     // ----- 削除画面 -----
-    @GetMapping("/delete")
+    /*@GetMapping("/delete")
     public String deleteCountryForm(@RequestParam(name = "code", required = false) String code, Model model) {
         // 削除対象の国コードをModelに登録
         model.addAttribute("countryCode", code);
@@ -62,15 +62,17 @@ public class CountryController {
         model.addAttribute("backToListUrl", "/country/list");
         // country/delete.htmlに画面遷移
         return "country/delete";
+    }*/
+
+ // ----- 削除 -----
+    @GetMapping(value = { "/delete", "/delete/{code}" })
+    public String deleteCountryForm(@PathVariable(name = "code", required = false) String code, Model model) {
+        //Country country = code != null ? service.getCountry(code) : new Country();
+        // 削除対象の国コードをModelに登録
+        model.addAttribute("countryCode", code);
+        // 削除画面から一覧画面に戻るリンクを追加
+        model.addAttribute("backToListUrl", "/country/list");
+        return "country/delete";
     }
 
-    // ----- 削除 -----
-    @PostMapping("/delete")
-    public String deleteCountry(@RequestParam("code") String code, Model model) {
-        // 削除
-        service.deleteCountry(code);
-        return code;
-
-        // 一覧画面にリダイレクト
-    }
 }
